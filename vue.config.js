4e31df7fb1f1a5a39761fc002686c5123b2d538b
Vish Patel <vishpatel913@googlemail.com>
2020-03-20 08:35:15 +0000

    reverted breaking pwa changes

--- vue.config.js
+++ vue.config.js
@@ -8,33 +8,19 @@ module.exports = {
     msTileColor: themeConfig['@theme-grey'],
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'green',
+
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      skipWaiting: true,
+    },
+    manifestPath: 'manifest.json',
+    manifestOptions: {},
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
       appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
       maskIcon: 'img/icons/safari-pinned-tab.svg',
       msTileImage: 'img/icons/msapplication-icon-144x144.png',
-    },
-    workboxPluginMode: 'GenerateSW',
-    workboxOptions: {
-      skipWaiting: true,
-    },
-    manifestPath: 'manifest.json',
-    manifestOptions: {
-      icons: [
-        {
-          src: 'img/icons/android-chrome-maskable-192x192',
-          sizes: '196x196',
-          type: 'image/png',
-          purpose: 'maskable',
-        },
-        {
-          src: 'img/icons/android-chrome-maskable-152x152.png',
-          sizes: '152x152',
-          type: 'image/png',
-          purpose: 'maskable',
-        },
-      ],
     },
   },
   css: {
