41953c177a0cb99756f47a764f4151a5ddc0cc69
Vish Patel <vpatel@and.digital>
2020-03-20 15:53:35 +0000

    fixed config for tests

--- functions/config.js
+++ functions/config.js
@@ -1,14 +1,24 @@
 const functions = require("firebase-functions");
 const fs = require("fs");
 
-let config = functions.config().env;
-
-if (process.env.NODE_ENV !== "production") {
-  if (fs.existsSync("./.env.json")) {
-    const env = require("./.env.json");
-
-    config = env;
-  }
+let config;
+switch (process.env.NODE_ENV) {
+  case "production":
+    config = functions.config().env;
+    break;
+  case "test":
+    config = {
+      google: {
+        key: "meh",
+        maps_uri: "httpsomething"
+      }
+    };
+    break;
+  default:
+    if (fs.existsSync("./.env.json")) {
+      const env = require("./.env.json");
+      config = env;
+    }
 }
 
 module.exports = config;
