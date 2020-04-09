19a8c16603009d3447c1fb925d6e662bed434dc3
Vish Patel <vpatel@and.digital>
2020-04-09 11:52:30 +0100

    working apollo server

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,33 +1,33 @@
 import * as functions from 'firebase-functions';
-// import { ApolloServer, gql } from 'apollo-server-cloud-functions';
+import { ApolloServer, gql } from 'apollo-server-cloud-functions';
 
 // Construct a schema, using GraphQL schema language
-// const typeDefs = gql`
-//   type Query {
-//     hello: String
-//   }
-// `;
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
 // Provide resolver functions for your schema fields
-// const resolvers = {
-//   Query: {
-//     hello: () => 'Hello world!',
-//   },
-// };
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
 
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
-
-// const app = server.createHandler({
-//   cors: {
-//     origin: '*',
-//     credentials: true,
-//   },
-// });
-
-// export const api = functions.https.onRequest(app);
-export const api = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!\n\n');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const app = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
+
+export const api = functions.https.onRequest(app);
+// export const api = functions.https.onRequest((request, response) => {
+//   response.send('Hello from Firebase!\n\n');
+// });
