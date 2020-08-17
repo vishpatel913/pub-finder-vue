5d31591408ecb905c7167903b243522e00402051
Vish Patel <vpatel@and.digital>
2020-08-17 15:11:03 +0100

    reverted package updates

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  presets: [['@vue/cli-plugin-babel/preset', {
-    useBuiltIns: 'usage',
-    polyfills: [
-      // module filenames from .\node_modules\core-js\modules\
-      'es.promise', // before "es6.promise"
-      'es.symbol', // before "es6.symbol"
-    ],
-  }]],
+  presets: [
+    '@vue/cli-plugin-babel/preset',
+  ],
 };
