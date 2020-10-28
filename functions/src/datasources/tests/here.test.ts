a2b30335259fc5f8a58aac4b36fada07d3f895fc
Vish Patel <vpatel@and.digital>
2020-10-28 12:17:30 +0000

    added search location finctionality to here service and resolver

--- functions/src/datasources/tests/here.test.ts
+++ functions/src/datasources/tests/here.test.ts
@@ -1,5 +1,6 @@
 import Here from '../here';
 import { geocodingMockResponse } from './mocks';
+import { Location } from '../../schemas';
 
 const here = new Here();
 const mocks = {
@@ -15,7 +16,7 @@ describe('[Here]', () => {
 });
 
 describe('[Here.getGeocoding]', () => {
-  let response;
+  let response: Location;
   beforeEach(async () => {
     mocks.get.mockReturnValueOnce(geocodingMockResponse);
     response = await here.getGeocoding({ lat: 7, lng: 12 });
@@ -24,6 +25,7 @@ describe('[Here.getGeocoding]', () => {
   it('returns an object with the correct keys', () => {
     expect(Object.keys(response)).toEqual(
       expect.arrayContaining([
+        'coords',
         'address',
         'district',
         'city',
@@ -36,12 +38,37 @@ describe('[Here.getGeocoding]', () => {
   it('returns the geocoding data in the correct format', () => {
     expect(response).toEqual(
       expect.objectContaining({
+        coords: { lat: 51.46215, lng: -0.16971 },
         address: '66 Sisters Avenue, London, SW11 1, England',
         district: 'Battersea',
         city: 'London',
         county: 'London',
         postalCode: 'SW11',
       })
+    );
+  });
+});
+
+describe('[Here.getSearchResults]', () => {
+  let response: Location[];
+  beforeEach(async () => {
+    mocks.get.mockReturnValueOnce(geocodingMockResponse);
+    response = await here.getSearchResults('test query');
+  });
+
+  it('returns the search results data in the correct format', () => {
+    expect(response).toHaveLength(1);
+    expect(response).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          coords: { lat: 51.46215, lng: -0.16971 },
+          address: '66 Sisters Avenue, London, SW11 1, England',
+          district: 'Battersea',
+          city: 'London',
+          county: 'London',
+          postalCode: 'SW11',
+        }),
+      ])
     );
   });
 });
