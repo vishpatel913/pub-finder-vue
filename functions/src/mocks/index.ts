dc24f9dc1f122559bc772eab3514ae6bbfd644cf
Vish Patel <vpatel@and.digital>
2020-10-29 14:58:37 +0000

    used coord inputs to generate faker mocks÷

--- functions/src/mocks/index.ts
+++ functions/src/mocks/index.ts
@@ -1,28 +1,30 @@
 import faker from 'faker';
 import { DateTime } from 'luxon';
 import { MockList } from 'apollo-server-cloud-functions';
-import { Pub, Location } from '../schemas';
+import { Location } from '../schemas';
+import { CoordsInput } from '../resolvers/types';
 
 export const mocks = {
   Int: (): number => 7,
   String: (): string => 'Something about mocks',
   Query: (): unknown => ({
-    pubs: (parent: Pub, args: { first: number }) => new MockList(args.first),
+    pubs: (_: unknown, args: { first: number }) => new MockList(args.first),
+    searchLocation: () => new MockList(5),
   }),
-  Location: (): Location => ({
+  Location: (_: unknown, args: { [key: string]: CoordsInput }): Location => ({
     coords: {
-      lat: parseFloat(faker.address.latitude()),
-      lng: parseFloat(faker.address.longitude()),
+      lat: args?.coords?.lat || parseFloat(faker.address.latitude()),
+      lng: args?.coords?.lng || parseFloat(faker.address.latitude()),
     },
     address: faker.address.streetAddress(),
     district: faker.address.city(),
     city: faker.fake('{{address.cityPrefix}}{{address.citySuffix}}'),
     county: faker.address.county(),
     postalCode: faker.fake(
-      '{{address.stateAbbr}}{{random.number({"min":1,"max":20})}}'
+      '{{address.stateAbbr}}{{random.number({"min":1,"max":29})}}'
     ),
   }),
-  Pub: (): Pub => ({
+  Pub: (): unknown => ({
     id: faker.random.uuid(),
     name: faker
       .fake('The {{commerce.productAdjective}} {{commerce.product}}')
@@ -34,12 +36,12 @@ export const mocks = {
     },
     rating: faker.random.number({ min: 0, max: 5, precision: 0.1 }),
     priceLevel: faker.random.number({ min: 1, max: 5 }),
-    directions: {
+    directions: (_: unknown, args: { from: CoordsInput }) => ({
       distance: faker.random.number({ min: 1, max: 1000 }),
       duration: faker.random.number({ min: 1, max: 1200 }),
       bearing: faker.random.number({ min: 1, max: 360, precision: 0.01 }),
-      link: `https://www.google.com/maps/dir/?api=1&origin=51.46095,-0.13922&destination=encodedname&destination_place_id=ChIJvZviab0FdkgR8w4do7vbgy4&travelmode=walking`,
-    },
+      link: `https://www.google.com/maps/dir/?api=1&origin=${args.from.lat},${args.from.lng}&destination=encodedname&destination_place_id=ChIJvZviab0FdkgR8w4do7vbgy4&travelmode=walking`,
+    }),
     openTimes: [
       {
         open: {
